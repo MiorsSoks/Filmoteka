c0e0a0f50ad5f6ec55631d66b97770f14c2320c3
Inna Klyuchnyk <Innysiaa@ukr.net>
2021-12-09 17:39:05 +0200

    Put genres in local storage

--- src/js/getGenresNames.js
+++ src/js/getGenresNames.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
+const API_KEY = '3b94c1b54af7d429587ecf26a37007c0';
+
 export async function getGenresNames() {
   try {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/genre/movie/list?api_key=3b94c1b54af7d429587ecf26a37007c0&language=en-US`,
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`,
     );
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(error);
   }
 }
+getGenresNames().then(data => {
+  localStorage.setItem('allGenres', JSON.stringify(data.genres));
+});
+const savedGenres = localStorage.getItem('allGenres');
+const arrayOfAllGenres = JSON.parse(savedGenres);
+console.log(arrayOfAllGenres);
