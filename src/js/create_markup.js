225d91b340cec36e8782229e3190cb8bcff24774
Inna Klyuchnyk <Innysiaa@ukr.net>
2021-12-10 00:36:56 +0200

    Add white picture

--- src/js/create_markup.js
+++ src/js/create_markup.js
@@ -3,43 +3,68 @@ const filmList = document.querySelector('.gallery-container');
 
 const BASE_IMG_URL = 'https://image.tmdb.org/t/p';
 const SIZE_IMG = '/w500';
+const noPosterImg = 'https://sd.keepcalms.com/i/sorry-no-picture-available-2.png';
 
 export function renderMarkup(movies) {
   const markup = movies.map(movie => createMarkup(movie)).join('');
   filmList.insertAdjacentHTML('beforeend', markup);
 }
 
-function createMarkup({ poster_path, title, genre_ids, release_date, popularity }) {
+function createMarkup({
+  poster_path,
+  title,
+  genre_ids,
+  release_date,
+  vote_average,
+  first_air_date,
+  name,
+}) {
   return `<li class="gallery-container__item">
         <a href="" class="link gallery-art" data-modal-open>
             <div class="film-img">
                             <picture>
                                 <source media="(min-width: 1024px)"
-                                    srcset="${BASE_IMG_URL + SIZE_IMG + poster_path} 1x, ${
-    BASE_IMG_URL + SIZE_IMG + poster_path
+                                    srcset="${
+                                      poster_path === null
+                                        ? noPosterImg
+                                        : BASE_IMG_URL + SIZE_IMG + poster_path
+                                    } 1x, ${
+    poster_path === null ? noPosterImg : BASE_IMG_URL + SIZE_IMG + poster_path
   } 2x">
                                 <source media="(min-width: 768px)"
-                                    srcset="${BASE_IMG_URL + SIZE_IMG + poster_path} 1x, ${
-    BASE_IMG_URL + SIZE_IMG + poster_path
+                                    srcset="${
+                                      poster_path === null
+                                        ? noPosterImg
+                                        : BASE_IMG_URL + SIZE_IMG + poster_path
+                                    } 1x, ${
+    poster_path === null ? noPosterImg : BASE_IMG_URL + SIZE_IMG + poster_path
   } 2x">
                                 <source media="(min-width: 320px)"
-                                    srcset="${BASE_IMG_URL + SIZE_IMG + poster_path} 1x, ${
-    BASE_IMG_URL + SIZE_IMG + poster_path
+                                    srcset="${
+                                      poster_path === null
+                                        ? noPosterImg
+                                        : BASE_IMG_URL + SIZE_IMG + poster_path
+                                    } 1x, ${
+    poster_path === null ? noPosterImg : BASE_IMG_URL + SIZE_IMG + poster_path
   } 2x">
-                            
+
                                 <img class="poster" src="${
-                                  BASE_IMG_URL + SIZE_IMG + poster_path
+                                  poster_path === null
+                                    ? noPosterImg
+                                    : BASE_IMG_URL + SIZE_IMG + poster_path
                                 }" alt="Poster of movie">
                             </picture>
             </div>
  <div class="film-description">
-      <h2 class="film-name">${title}</h2>
+      <h2 class="film-name">${title ? title : name}</h2>
       <div class="film-info">
           <p class="genres">${genre_ids}</p>
-          <p class="year">${release_date.slice(0, 4)}</p>
-          <p class="rating">${popularity.toFixed(1)}</p>
+          <p class="year">${
+            release_date ? release_date.slice(0, 4) : first_air_date.slice(0, 4)
+          }</p>
+          <p class="rating">${vote_average}</p>
       </div>
-    
+
         </div>
         </a>
 </li>`;
--- src/js/search_trend_collection.js
+++ src/js/search_trend_collection.js
@@ -1,6 +1,9 @@
-import { fetchCollection } from "./bring_trend_collection";
-import { renderCollection } from "./create_render_collection";
+import { fetchCollection } from './bring_trend_collection';
+import { renderCollection } from './create_render_collection';
+import { renderMarkup } from './create_markup';
 
 window.addEventListener('load', () => {
-    fetchCollection().then(renderCollection);
+  fetchCollection().then(data => {
+    renderMarkup(data.results);
+  });
 });
