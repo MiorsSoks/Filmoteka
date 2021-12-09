958914f99dd72c05f32184a1283c1a9b614c8db1
KhrystynaLavryniv <84865514+KhrystynaLavryniv@users.noreply.github.com>
2021-12-09 12:15:58 +0200

    fix footer

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -3,12 +3,28 @@
     openModal: document.querySelector('[data-footer-modal-open]'),
     closeModalBtn: document.querySelector('[data-footer-modal-close]'),
     modal: document.querySelector('[data-footer-modal]'),
+    back: document.querySelector('.footer-backdrop'),
+    m:document.querySelector('.footer-modal'),
   };
 
   refs.openModal.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
+  refs.m.addEventListener('click', (e) => {
+    e.stopPropagation(), e.stopImediatePropagation
+  })
+
+  window.addEventListener('keydown', (e) => {
+   if (e.key === 'Escape') {
+      console.log("keydown")
+      toggleModal()  
+    }
+  })
+
+
+  refs.back.addEventListener('click',toggleModal)
+  
+  function toggleModal(event) {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
