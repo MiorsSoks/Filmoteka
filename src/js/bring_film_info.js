a75a12e7b940b0e23ed21fb1721a290c302bc373
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2021-12-10 20:21:00 +0200

    correct

---
+++ src/js/bring_film_info.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+let filmId = 71446;
+
+export async function fetchFilmInfo() {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${filmId}?api_key=3b94c1b54af7d429587ecf26a37007c0&language=en-US`,
+      );
+  
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }m
--- src/js/search_trend_collection.js
+++ src/js/search_trend_collection.js
@@ -2,5 +2,9 @@ import { fetchCollection } from "./bring_trend_collection";
 import { renderCollection } from "./create_render_collection";
 
 window.addEventListener('load', () => {
-    fetchCollection().then(renderCollection);
+    fetchCollection().then(data => {
+        renderCollection(data.results);
+      });
 });
+
+
