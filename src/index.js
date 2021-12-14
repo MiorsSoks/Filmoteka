51c730f916fb38ed6c6b9077e13302bf153dfb92
Mykola-Veres <veresnickolay@gmail.com>
2021-12-14 12:11:06 +0200

    new

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ import './js/search_trend_collection';
 import './js/markup-modal';
 import './js/dark-light-theme';
 // import './js/add_to_library';
-import './js/library-pagination';
+// import './js/library-pagination';
 import './js/btns_watched_queue';
